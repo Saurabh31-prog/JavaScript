9c85b1b205c25d314e6ea1370742526340bee741
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-05-02 07:50:44 +0000

    Arrays splic and slice difference

---
+++ Array/01_arrays.js
@@ -0,0 +1,52 @@
+//array
+const myArr = [0, 1, 2, 3, 4, 5];
+const stringArr =[ 'hello', 'world'];
+const myArr2 = new Array(1, 2, 3, 4, 5);
+
+/*
+//Arrays methods
+
+myArr.push(33);  //add element to the array
+console.log(myArr);
+myArr.pop(3);
+console.log(myArr); //remove last element
+
+
+myArr.unshift(9);
+console.log(myArr); //add element to the beginning
+myArr.shift();
+console.log(myArr); //remove first element
+
+const newArr = myArr.join();
+
+console.log(newArr);
+*/
+
+
+
+//slice, splice IMPORTANT******
+
+/*
+console.log("a", myArr);
+
+const myn1 = myArr.slice(1, 3); 
+
+console.log(myn1);
+console.log("b", myArr);
+
+const myn2 = myArr.splice(1, 3);
+console.log("c", myArr);
+console.log(myn2);
+*/
+
+//slice vs splice
+/*
+let arr1 = [0, 1, 2, 3, 4, 5];
+const slicedArr = arr1.slice(0, 3);
+console.log(slicedArr); // [0, 1, 2]
+console.log(arr1); // [0, 1, 2, 3, 4, 5]
+
+const splicedArr = arr1.splice(0, 3);
+console.log(splicedArr); // [0, 1, 2]
+console.log(arr1); // [3, 4, 5]
+*/
