3b300ed5f8568a28c6b81914795e4e1a85b360aa
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-05-17 18:35:29 +0000

    resolve and reject, finally

--- Advance/promises.js
+++ Advance/promises.js
@@ -25,3 +25,47 @@ new Promise(function(resolve, reject){
 }).then(function(){
     console.log("async 2 is resolved")
 })
+
+//third promise
+
+const thirdPromise = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({
+            username:"code",
+            email : "code@gmail.com"
+        })
+    }, 1000)
+})  
+
+thirdPromise.then(function(user){
+    console.log(user); //prints the complete object
+})
+
+
+//fourth promise
+
+const fourthpromise = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username: "saurabh", password: "123"})
+        }else{
+            reject('errro: something went wrong');
+        }
+    }, 1000)
+})
+
+fourthpromise.then((user) =>{
+    console.log(user);
+    return user.username;
+}).then((username) =>{
+console.log(username);
+}).catch(function(error){
+    console.log(error)
+}).finally(() =>{
+    console.log("promise is resolved or rejected ")
+})
+
+
+//promise five
+
