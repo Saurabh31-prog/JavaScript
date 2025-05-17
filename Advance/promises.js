d979de71857d09456395243938c9916754f319ac
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-05-17 18:20:53 +0000

    Promise and resolve()

--- Advance/promises.js
+++ Advance/promises.js
@@ -1,10 +1,27 @@
+/*
 const promiseOne = new Promise(function(resolve, reject){
     //do an async task
     //db calls, cryptography , network calls 
 
     setTimeout(function(){
         console.log('Async task is complete');
-    }, 1000);
+        resolve()
+    }, 3000);
 })
 
-promiseOne.then()
+//first setimeout will execute and then resolve() then lower function will execute
+promiseOne.then(function(){
+    console.log('promise comsumed');
+})
+
+*/
+
+//same code as above but more efficient
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("async task 2");
+        resolve();
+    }, 1000)
+}).then(function(){
+    console.log("async 2 is resolved")
+})
