d06633e2abc6b9d9f45ac940fd63609746f6b61a
Saurabh Mungle <munglesaurabh7284@gmail.com>
2025-05-18 08:05:48 +0000

    Promises , async , await

--- Advance/promises.js
+++ Advance/promises.js
@@ -1,3 +1,19 @@
+//Promises
+/*
+The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+
+Description
+A Promise is a proxy for a value not necessarily known when the promise is created. It allows you to associate handlers with an asynchronous action's eventual success value or failure reason. This lets asynchronous methods return values like synchronous methods: instead of immediately returning the final value, the asynchronous method returns a promise to supply the value at some point in the future.
+
+A Promise is in one of these states:
+
+pending: initial state, neither fulfilled nor rejected.
+fulfilled: meaning that the operation was completed successfully.
+rejected: meaning that the operation failed.
+
+*/
+
+
 /*
 const promiseOne = new Promise(function(resolve, reject){
     //do an async task
@@ -69,3 +85,50 @@ console.log(username);
 
 //promise five
 
+const promiseFive =  new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true;
+
+        if(!error){
+            resolve({
+                username: "Saurabh",
+                password: "1234"
+            })
+        }else{
+            reject("error: js went wrong"); 
+        }
+    }, 1000)
+});
+async function consumePromiseFive() {
+    try{const response = await promiseFive
+    console.log(response);
+    }catch(error) {
+        console.log("error")
+    }
+}
+consumePromiseFive()
+
+//-----------------------------------------
+
+// async function getAllUsers() {
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//     const data = await response.json()
+//     console.log(data);
+//     }catch(error){
+//         console.log("error response");
+//     }
+// }
+// getAllUsers();
+
+
+//same format in .then 
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) =>{
+return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
